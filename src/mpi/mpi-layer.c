1766f7443f0028fbc8a7d89dca1c18a5a5438b2f
Moritz Eckert <mo@mightym0.de>
2018-06-12 17:01:54 +0200

    Add cracking part to node_logic

--- src/mpi/mpi-layer.c
+++ src/mpi/mpi-layer.c
@@ -4,16 +4,16 @@
 #include <sys/wait.h>
 
 #include "mpi-layer.h"
-#include "../distribution/input/vector.h"
-#include "../distribution/input/read_file.h"
+#include "../input/input.h"
 #include "../distribution/distribution.h"
+#include "../cracker/cracker.h"
 
 int mpi_start (int argc, char* argv[], char *wlist, char *plist) {
 	int rank, size;
 
-	MPI_Init(&argc, &argv);      /* starts MPI */
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);        /* get current process id */
-	MPI_Comm_size(MPI_COMM_WORLD, &size);        /* get number of processes */
+	MPI_Init(&argc, &argv);		 /* starts MPI */
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);		 /* get current process id */
+	MPI_Comm_size(MPI_COMM_WORLD, &size);		 /* get number of processes */
 
 	printf("Process %d of %d: Hello!\n", rank, size);
 	node_logic(rank, size, wlist, plist);
@@ -28,7 +28,9 @@ int mpi_start (int argc, char* argv[], char *wlist, char *plist) {
 void node_logic (int rank, int size, char *wlist, char *plist) {
 	FILE *word_fp, *pass_fp;
 	vector *words, words_dist, *pass;
-    size_t file_size;
+	size_t file_size;
+	int hash_size, w, h;
+	hash_element *hashes;
 
 	word_fp = open_file(wlist, &file_size);
 	words = index_file(word_fp, file_size);
@@ -37,8 +39,23 @@ void node_logic (int rank, int size, char *wlist, char *plist) {
 	pass_fp = open_file(plist, &file_size);
 	pass = index_file(pass_fp, file_size);
 
+	 if ((hashes = parse_passfile(pass, &hash_size)) == NULL ) {
+		exit(EXIT_FAILURE);
+	}
+
+	for (w = 0; w < total_vector(&words_dist); w++) {
+		for (h = 0; h < hash_size; h++) {
+			if (hashes[h].done) {
+				continue;
+			}
+			if (check_pass (&hashes[h].ctx, get_vector(&words_dist, w), hashes[h].salt, hashes[h].hash)) {
+				hashes[h].done = 1;
+				printf("PID[%d]: (%s)%s == %s.\n", rank, hashes[h].salt, get_vector(&words_dist, w), hashes[h].hash);
+			}
+		}
+	}
+
 
 	//DEBUGGIN info
 	printf("Prozess %d hat %d Woerter.\n", rank, total_vector(&words_dist));
-	printf("Prozess %d hat %d Hashes.\n", rank, total_vector(pass));
 }
