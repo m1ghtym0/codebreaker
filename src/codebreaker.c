d02048a5c4998371bd49525f40c8379f54edf429
Moritz Eckert <mo@mightym0.de>
2018-06-21 16:06:37 +0200

    Add parameter to specify hashing format

--- src/codebreaker.c
+++ src/codebreaker.c
@@ -6,16 +6,20 @@
 #include"mpi/mpi-layer.h"
 
 void usage(char *argv[]) {
-    fprintf(stderr, "Usage: %s -w <wordlist> -p <passlist>\n", argv[0]);
+	fprintf(stderr, "Usage: %s -w wordlist -p passlist [-f format] \n", argv[0]);
+	fprintf(stderr, "-w wordlist: wordlist for word based attack\n");
+	fprintf(stderr, "-p passlist: passlist containing hashes to be cracked\n");
+	fprintf(stderr, "-f format: Specify hash-format of passlist regular, linux-crypt (default: regular) \n");
 }
 int main(int argc, char* argv[]) {
 	char *wordlist = NULL;
 	char *passlist = NULL;
+	char *format = NULL;
 	char c;
 
 	opterr = 0;
 
-	while ((c = getopt (argc, argv, "w:p:")) != -1) {
+	while ((c = getopt (argc, argv, "w:p:f:")) != -1) {
 		switch (c) {
 			case 'w':
 				wordlist = optarg;
@@ -23,8 +27,11 @@ int main(int argc, char* argv[]) {
 			case 'p':
 				passlist = optarg;
 				break;
+			case 'f':
+				format = optarg;
+				break;
 			case '?':
-				if (optopt == 'w' || optopt == 'p') {
+				if (optopt == 'w' || optopt == 'p' || optopt == 'f') {
 					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
 				} else if (isprint (optopt)) {
 					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
@@ -42,8 +49,12 @@ int main(int argc, char* argv[]) {
 		usage(argv);
 		exit(EXIT_FAILURE);
 	}
+	
+	if (format == NULL) {
+		format = "regular";
+	}
 
 	//MPI TESTER
-	mpi_start(argc, argv, wordlist, passlist);
+	mpi_start(argc, argv, wordlist, passlist, format);
 	return 0;
 }
