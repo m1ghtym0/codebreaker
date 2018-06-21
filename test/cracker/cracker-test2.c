2797c9c0f88eaef8f4ce4b456ba59b677a03e57b
Moritz Eckert <mo@mightym0.de>
2018-06-21 10:33:31 +0200

    Fix cracker-test

--- test/cracker/cracker-test2.c
+++ test/cracker/cracker-test2.c
@@ -7,22 +7,23 @@
 
 
 int main (int argc, char *argv[]) {
-    char *wlist, *plist;
-    int w, h, hash_size;
+	char *wlist, *plist;
+	int w, h, hash_size;
 	FILE *words_fp, *pass_fp;
 	vector *words, *pass;
 	size_t file_size;
 	hash_element *hashes;
+	int *done;
 
-     
-    if (argc < 3) {
-        return EXIT_FAILURE;
-    }
-    
-    wlist = argv[1];
-    plist = argv[2];
+	 
+	if (argc < 3) {
+		return EXIT_FAILURE;
+	}
+	
+	wlist = argv[1];
+	plist = argv[2];
    
-    
+	
    
 	words_fp = open_file(wlist, &file_size);
 	words = index_file(words_fp, file_size);
@@ -30,21 +31,25 @@ int main (int argc, char *argv[]) {
 	pass_fp = open_file(plist, &file_size);
 	pass = index_file(pass_fp, file_size);
 
-    if ((hashes = parse_passfile(pass, &hash_size)) == NULL ) {
-        return EXIT_FAILURE;
-    }
-    
+	if ((hashes = parse_passfile(pass, &hash_size)) == NULL ) {
+		return EXIT_FAILURE;
+	}
+   
+	if ((done = calloc(hash_size, sizeof(int))) == NULL) {
+		perror("calloc");
+		return EXIT_FAILURE;
+	}
 
 	for (w = 0; w < total_vector(words); w++) { 
 		for (h = 0; h < hash_size; h++) {
 			if (check_pass (&hashes[h].ctx, get_vector(words, w), hashes[h].salt, hashes[h].hash)) {
-				hashes[h].done = 1;
+				done[h] = 1;
 			}
 		}
 	}
 
 	for (h = 0; h < hash_size; h++) {
-		if (!hashes[h].done) {
+		if (!done[h]) {
 			return EXIT_FAILURE;
 		}
 	}
